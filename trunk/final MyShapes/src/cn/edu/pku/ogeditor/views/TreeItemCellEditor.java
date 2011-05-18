1def55c162774d9cd4b7d8bfc11eb4eec76a5f4c
crazyxxy@gmail.com <crazyxxy@gmail.com@56849ee1-43d4-cff7-9437-04d1214a22df>
2011-05-18 01:34:13 +0000

    合并

---
+++ trunk/final MyShapes/src/cn/edu/pku/ogeditor/views/TreeItemCellEditor.java
@@ -0,0 +1,33 @@
+package cn.edu.pku.ogeditor.views;
+
+import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+
+public class TreeItemCellEditor extends CellEditor {
+
+	@Override
+	protected Control createControl(Composite parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Object doGetValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void doSetFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void doSetValue(Object value) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
