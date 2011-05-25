c8dea4e4ef9ca5ef96edc2695734efc0667bc834
crazyxxy@gmail.com <crazyxxy@gmail.com@56849ee1-43d4-cff7-9437-04d1214a22df>
2011-05-25 05:19:59 +0000

    加入一个testView

---
+++ trunk/final MyShapes/src/cn/edu/pku/ogeditor/views/TestView.java
@@ -0,0 +1,33 @@
+package cn.edu.pku.ogeditor.views;
+
+import org.eclipse.draw2d.FigureCanvas;
+import org.eclipse.gef.EditDomain;
+import org.eclipse.gef.editparts.ScalableFreeformRootEditPart;
+import org.eclipse.gef.ui.parts.ScrollingGraphicalViewer;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+import cn.edu.pku.ogeditor.ShapesEditor;
+import cn.edu.pku.ogeditor.model.ShapesDiagram;
+import cn.edu.pku.ogeditor.parts.ShapesEditPartFactory;
+
+public class TestView extends ViewPart {
+ScrollingGraphicalViewer graphicalViewer;
+FigureCanvas canvas;
+ShapesDiagram diagram;
+public void createPartControl(Composite parent) {
+    graphicalViewer = new ScrollingGraphicalViewer();
+    canvas = (FigureCanvas) graphicalViewer.createControl(parent);
+    ScalableFreeformRootEditPart root = new ScalableFreeformRootEditPart();
+    graphicalViewer.setRootEditPart(root);
+    graphicalViewer.setEditDomain(new EditDomain());
+    graphicalViewer.setEditPartFactory(new ShapesEditPartFactory());
+    graphicalViewer.setContents(ShapesEditor.myselfShapesEditor.getDiagram());
+}
+
+@Override
+public void setFocus() {
+	// TODO Auto-generated method stub
+	
+}
+}
