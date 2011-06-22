f4e37ff0105384655932e8fb335c0f1d0aca0fcb
crazyxxy@gmail.com <crazyxxy@gmail.com@56849ee1-43d4-cff7-9437-04d1214a22df>
2011-06-22 02:58:43 +0000

    改正一些警告

--- trunk/final MyShapes/src/cn/edu/pku/ogeditor/properties/ConnectionBasicSection.java
+++ trunk/final MyShapes/src/cn/edu/pku/ogeditor/properties/ConnectionBasicSection.java
@@ -11,62 +11,6 @@ import org.eclipse.ui.views.properties.tabbed.AbstractPropertySection;
 import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
 
 public class ConnectionBasicSection extends AbstractPropertySection {
-//
-//    private Text nameText;
-//
-//    private ConnectionEditPart cep;
-//    public ConnectionBasicSection() {
-//		// TODO Auto-generated constructor stub
-//	}
-//    private ModifyListener listener = new ModifyListener() {
-//
-//        public void modifyText(ModifyEvent arg0) {
-//        	cep.getCastedModel().setName(nameText.getText());
-//        }
-//    };
-//
-//    public void setInput(IWorkbenchPart part, ISelection selection) {
-//        super.setInput(part, selection);
-//        if(!(selection instanceof IStructuredSelection))
-//        {
-//        	return;
-//        }
-//        Object input = ((IStructuredSelection) selection).getFirstElement();
-//        if(input instanceof ConnectionEditPart)
-//        	cep = (ConnectionEditPart) input;
-//    }
-//
-//    public void createControls(Composite parent,
-//            TabbedPropertySheetPage aTabbedPropertySheetPage) {
-//        super.createControls(parent, aTabbedPropertySheetPage);
-//        Composite composite = getWidgetFactory()
-//            .createFlatFormComposite(parent);
-//        FormData data;
-//
-//        nameText = getWidgetFactory().createText(composite, ""); //$NON-NLS-1$
-//        data = new FormData();
-//        data.left = new FormAttachment(0, STANDARD_LABEL_WIDTH);
-//        data.right = new FormAttachment(100, 0);
-//        data.top = new FormAttachment(0, ITabbedPropertyConstants.VSPACE);
-//        nameText.setLayoutData(data);
-//        nameText.addModifyListener(listener);
-//
-//        CLabel labelLabel = getWidgetFactory()
-//            .createCLabel(composite, "name:"); //$NON-NLS-1$
-//        data = new FormData();
-//        data.left = new FormAttachment(0, 0);
-//        data.right = new FormAttachment(nameText,
-//            -ITabbedPropertyConstants.HSPACE);
-//        data.top = new FormAttachment(nameText, 0, SWT.CENTER);
-//        labelLabel.setLayoutData(data);
-//    }
-//
-//    public void refresh() {
-//        nameText.removeModifyListener(listener);
-//        nameText.setText(cep.getCastedModel().getName());
-//        nameText.addModifyListener(listener);
-//    }
-
 
 	private PropertySheetPage page;
 
