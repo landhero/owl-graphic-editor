a51fc87ae2b5aa679680bb429c31dc3e905c95a6
wodeyouxiang010@gmail.com <wodeyouxiang010@gmail.com@56849ee1-43d4-cff7-9437-04d1214a22df>
2011-07-12 13:41:09 +0000

    增加了Concept Outline

---
+++ trunk/final MyShapes/src/cn/edu/pku/ogeditor/views/ConceptOutlineLabelProvider.java
@@ -0,0 +1,49 @@
+package cn.edu.pku.ogeditor.views;
+
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.swt.graphics.Image;
+
+import cn.edu.pku.ogeditor.model.Shape;
+import cn.edu.pku.ogeditor.model.ShapesDiagram;
+
+public class ConceptOutlineLabelProvider implements ILabelProvider {
+
+	@Override
+	public void addListener(ILabelProviderListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isLabelProperty(Object element, String property) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void removeListener(ILabelProviderListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Image getImage(Object element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getText(Object element) {
+		// TODO Auto-generated method stub
+		Shape shape = (Shape) element;
+		return shape.getName();
+	}
+
+}
